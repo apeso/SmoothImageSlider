079fb9f90bb3ce8502d4b24072e8e15a3e6e5b35
Antonia <antonia.peso@icloud.com>
2024-06-17 22:50:02 +0200

    Ispravak

--- script.js
+++ script.js
@@ -1,62 +1,66 @@
 $(document).ready(function() {
-    var totalImages = 9;
-    var images = [];
+    var $sliderTopRow = $('#slider-top-row');
+    var $sliderBottomRow=$('#slider-bottom-row');
 
-    for (var i = 1; i <= totalImages; i++) {
-        images.push('assets/slider-image-' + i + '.jpg');
-    }
-
-    function renderImages() {
-        var sliderRow1 = $('#slider-row-1');
-        var sliderRow2 = $('#slider-row-2');
-        sliderRow1.empty();
-        sliderRow2.empty();
-
-        for (var i = 0; i < 5; i++) {
-            var img = $('<img>').attr('src', images[i]).attr('alt', 'Slika ' + (i + 1));
-            sliderRow1.append(img);
-        }
-
-        for (var i = 5; i < 9; i++) {
-            var img = $('<img>').attr('src', images[i]).attr('alt', 'Slika ' + (i + 1));
-            sliderRow2.append(img);
-        }
-
-        var leftArrowImg = $('<img>').attr('src', 'assets/arrow-blue-left.png').addClass('arrow').attr('id', 'left-arrow');
-        var rightArrowImg = $('<img>').attr('src', 'assets/arrow-blue-right.png').addClass('arrow').attr('id', 'right-arrow');
-
-        var arrowsDiv = $('<div>').addClass('arrows');
-        arrowsDiv.append(leftArrowImg).append(rightArrowImg);
-        sliderRow2.append(arrowsDiv);
-
-        $('#left-arrow').on('click', function() {
-            moveSlider('left');
-        });
-
-        $('#right-arrow').on('click', function() {
-            moveSlider('right');
-        });
-    }
-
-    renderImages();
+    var $imagesTopRow = $sliderTopRow.find('img');
+    var $imagesBottomRow=$sliderBottomRow.find('img');
 
     function moveSlider(direction) {
+        
         if (direction === 'right') {
-            var lastImage = images.pop();
-            images.unshift(lastImage);
+            
+            var distanceTopRow = $imagesTopRow.last().width()+10;
+            $sliderTopRow.css('transition', 'transform 1s ease');
+            $sliderTopRow.css('transform', 'translateX(' + distanceTopRow+ 'px)');
+
+            var distanceBottomRow = $imagesBottomRow.last().width()+10;
+            $sliderBottomRow.css('transition', 'transform 1s ease');
+            $sliderBottomRow.css('transform', 'translateX(' + distanceBottomRow+ 'px)');
+
+            setTimeout(function() {
+        
+                $sliderTopRow.prepend($imagesTopRow.last());
+                $sliderTopRow.css('transition', 'none');
+                $sliderTopRow.css('transform', 'none');
+                $imagesTopRow = $sliderTopRow.find('img');
+
+                $sliderBottomRow.prepend($imagesBottomRow.last());
+                $sliderBottomRow.css('transition', 'none');
+                $sliderBottomRow.css('transform', 'none');
+                $imagesBottomRow = $sliderBottomRow.find('img');
+            }, 1000);
         } else if (direction === 'left') {
-            var firstImage = images.shift();
-            images.push(firstImage);
+            
+            var distanceTopRow = $imagesTopRow.first().width()+10;
+            $sliderTopRow.css('transition', 'transform 1s ease');
+            $sliderTopRow.css('transform', 'translateX(-' + distanceTopRow + 'px)');
+
+            var distanceBottomRow = $imagesBottomRow.first().width()+10;
+            $sliderBottomRow.css('transition', 'transform 1s ease');
+            $sliderBottomRow.css('transform', 'translateX(-' + distanceBottomRow + 'px)');
+
+            setTimeout(function() {
+
+                $sliderTopRow.append($imagesTopRow.first());
+                $sliderTopRow.css('transition', 'none');
+                $sliderTopRow.css('transform', 'none');
+                $imagesTopRow = $sliderTopRow.find('img');
+                
+                $sliderBottomRow.append($imagesBottomRow.first());
+                $sliderBottomRow.css('transition', 'none');
+                $sliderBottomRow.css('transform', 'none');
+                $imagesBottomRow = $sliderBottomRow.find('img');
+
+            }, 1000);
         }
-
-        $('.slider').addClass('transition');
-
-        setTimeout(function() {
-            renderImages();
-        }, 200);
-
-        setTimeout(function() {
-            $('.slider').removeClass('transition');
-        }, 700); 
     }
+
+    
+    $('#left-arrow').click(function() {
+        moveSlider('left');
+    });
+
+    $('#right-arrow').click(function() {
+        moveSlider('right');
+    });
 });
