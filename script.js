eca063be95374168a69c053dec198a52062a4ad1
Antonia <antonia.peso@icloud.com>
2024-06-19 22:37:33 +0200

    Final update

--- script.js
+++ script.js
@@ -1,66 +1,75 @@
-$(document).ready(function() {
-    var $sliderTopRow = $('#slider-top-row');
-    var $sliderBottomRow=$('#slider-bottom-row');
+$(document).ready(() => {
+    const $sliderTopRow = $('#slider-top-row');
+    const $sliderBottomRow = $('#slider-bottom-row');
 
-    var $imagesTopRow = $sliderTopRow.find('img');
-    var $imagesBottomRow=$sliderBottomRow.find('img');
+    let $imagesTopRow = $sliderTopRow.find('img');
+    let $imagesBottomRow = $sliderBottomRow.find('img');
 
-    function moveSlider(direction) {
-        
+    const moveSlider = (direction) => {
         if (direction === 'right') {
-            
-            var distanceTopRow = $imagesTopRow.last().width()+10;
-            $sliderTopRow.css('transition', 'transform 1s ease');
-            $sliderTopRow.css('transform', 'translateX(' + distanceTopRow+ 'px)');
+            const distanceTopRow = $imagesTopRow.last().width() + 10;
+            $sliderTopRow.css({
+                transition: 'transform 1s ease',
+                transform: `translateX(${distanceTopRow}px)`
+            });
 
-            var distanceBottomRow = $imagesBottomRow.last().width()+10;
-            $sliderBottomRow.css('transition', 'transform 1s ease');
-            $sliderBottomRow.css('transform', 'translateX(' + distanceBottomRow+ 'px)');
+            const distanceBottomRow = $imagesBottomRow.last().width() + 10;
+            $sliderBottomRow.css({
+                transition: 'transform 1s ease',
+                transform: `translateX(${distanceBottomRow}px)`
+            });
 
-            setTimeout(function() {
-        
+            setTimeout(() => {
                 $sliderTopRow.prepend($imagesTopRow.last());
-                $sliderTopRow.css('transition', 'none');
-                $sliderTopRow.css('transform', 'none');
+                $sliderTopRow.css({
+                    transition: 'none',
+                    transform: 'none'
+                });
                 $imagesTopRow = $sliderTopRow.find('img');
 
                 $sliderBottomRow.prepend($imagesBottomRow.last());
-                $sliderBottomRow.css('transition', 'none');
-                $sliderBottomRow.css('transform', 'none');
+                $sliderBottomRow.css({
+                    transition: 'none',
+                    transform: 'none'
+                });
                 $imagesBottomRow = $sliderBottomRow.find('img');
             }, 1000);
         } else if (direction === 'left') {
-            
-            var distanceTopRow = $imagesTopRow.first().width()+10;
-            $sliderTopRow.css('transition', 'transform 1s ease');
-            $sliderTopRow.css('transform', 'translateX(-' + distanceTopRow + 'px)');
+            const distanceTopRow = $imagesTopRow.first().width() + 10;
+            $sliderTopRow.css({
+                transition: 'transform 1s ease',
+                transform: `translateX(-${distanceTopRow}px)`
+            });
 
-            var distanceBottomRow = $imagesBottomRow.first().width()+10;
-            $sliderBottomRow.css('transition', 'transform 1s ease');
-            $sliderBottomRow.css('transform', 'translateX(-' + distanceBottomRow + 'px)');
+            const distanceBottomRow = $imagesBottomRow.first().width() + 10;
+            $sliderBottomRow.css({
+                transition: 'transform 1s ease',
+                transform: `translateX(-${distanceBottomRow}px)`
+            });
 
-            setTimeout(function() {
-
+            setTimeout(() => {
                 $sliderTopRow.append($imagesTopRow.first());
-                $sliderTopRow.css('transition', 'none');
-                $sliderTopRow.css('transform', 'none');
+                $sliderTopRow.css({
+                    transition: 'none',
+                    transform: 'none'
+                });
                 $imagesTopRow = $sliderTopRow.find('img');
-                
-                $sliderBottomRow.append($imagesBottomRow.first());
-                $sliderBottomRow.css('transition', 'none');
-                $sliderBottomRow.css('transform', 'none');
-                $imagesBottomRow = $sliderBottomRow.find('img');
 
+                $sliderBottomRow.append($imagesBottomRow.first());
+                $sliderBottomRow.css({
+                    transition: 'none',
+                    transform: 'none'
+                });
+                $imagesBottomRow = $sliderBottomRow.find('img');
             }, 1000);
         }
-    }
+    };
 
-    
-    $('#left-arrow').click(function() {
+    $('#left-arrow').click(() => {
         moveSlider('left');
     });
 
-    $('#right-arrow').click(function() {
+    $('#right-arrow').click(() => {
         moveSlider('right');
     });
 });
